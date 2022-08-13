4c20143efcd6553258de2133b0507482619dede3
Lucas Ramos <ramos.lucash@gmail.com>
2022-08-12 22:37:44 -0300

    style: decrease indent size

--- src/App.spec.tsx
+++ src/App.spec.tsx
@@ -3,8 +3,8 @@ import { render, screen } from '@testing-library/react';
 import App from '@/App';
 
 describe('App', () => {
-    it('should render "Vite + React"', () => {
-        render(<App />);
-        expect(screen.getByText('Vite + React'));
-    });
+  it('should render "Vite + React"', () => {
+    render(<App />);
+    expect(screen.getByText('Vite + React'));
+  });
 });
